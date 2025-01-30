1dfd0af757fab846a11cd9fefbe9bebb7416e771
ihawp <ihawpebay@gmail.com>
2025-01-29 18:16:42 -0600

    UI intact!

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
+const button = document.getElementById('submitInput');
+const chat = document.getElementById('chat');
+const input = document.getElementById('input');
+const click = async () => {
 
+    chat.innerHTML += `<p class="user">${input.value}</p>`;
 
-let button = document.getElementById('submitInput');
-let input = document.getElementById('input');
-
-
-button.addEventListener('click', async () => {
-
-    let host = 'localhost';
-
-    const response = await fetch(`http://${host}:11434/api/generate`, {
+    const response = await fetch(`http://localhost:11434/api/generate`, {
         method: 'POST',
-            headers: {
+        headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            model: 'deepseek-r1:8b',
+            model: 'deepseek-r1:1.5b',
             prompt: input.value,
             stream: false
         }),
     });
 
+    input.value = '';
+
     let data = await response.json();
 
+    console.log(data.response);
 
-    document.body.innerHTML += data.response;
+    chat.innerHTML += `<p class="ai">${data.response}</p>`;
+}
 
-    console.log(data);
-})
+button.addEventListener('click', click);
