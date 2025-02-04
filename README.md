12d35adf1b3fbf53890b5939296f5febeb51268b
ihawp <ihawpebay@gmail.com>
2025-02-04 14:04:39 -0600

    implementing readme changes and adding of C/exe for running app locally with more ease!

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # Local Ollama/Deepseek UI
 
+## Requirements:
++ Ollama
+  + Model installed (default is 'deepseek-coder3:)
+
 ## Setup:
-+ Install Ollama.
-+ In terminal, run:
++ Install Ollama (https://ollama.com/download)
++ In powershell, run:
   + ***ollama run deepseek-1:1.5b***
-  + ***ollama serve***
++ Download 'ihawp-local-deepseek' to your C:// directory
++ Put the executable whereever you want it.
++ Open the executable
++ Enjoy!?
+
 
 ## Change Models:
 Navigate to chat.js, inside the ***fetch*** change the url where the version is specified.
---
+++ ihawp-local-deepseek/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+void main();
+
+
+void main() {
+
+	const char *path = "C://ihawp-local-deepseek/ui/";
+
+
+	/*
+
+		Serve Ollama locally from Ollama (required local app and preinstalled LLM models 
+		and whatnot but of course you can change the models being used and what not in 
+		the JS files).
+
+		ExecutionPolicy needs to be set to RemoteSigned (I'll leave this up to the user)
+
+	*/
+	system("start powershell.exe ollama serve");
+
+
+	/*
+
+		Open the default port for the react project in users default browser.
+
+	*/
+	system("start powershell.exe start \"\" \"http://localhost:5173\"");
+
+
+	/*
+
+		Open a new Powershell window.
+		change to project path (this will have to change to make it useful app,
+					I'm sure there is a way to search the computer
+					for a project..? or something of the sort... without
+					having to make the user input in a powershell window the
+					path to the directory that 'npm run dev' should be run in).
+		Run 'npm run dev'.
+
+	*/
+	chdir(path);
+	system("start powershell.exe npm run dev");
+	system("start powershell.exe C://ihawp-local-deepseek/hidePowershellCMD.ps1");
+
+}
